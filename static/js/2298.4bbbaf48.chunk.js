a3fc46b560d1ce22863c567e0c4494cad744de68
zhipenglin <zhipenglin@users.noreply.github.com>
2026-08-21 06:54:09 +0000

    deploy: 246f96ce1d32e381d1059c0137c969968397ef02

--- static/js/2298.4bbbaf48.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(globalThis.webpackChunk_table_page_0_1_34||=[]).push([[2298],{70701(e,a,n){n.r(a);const t={TablePage:n(69666).A};n.d(a,["default",0,t,"manifest",0,{name:"table-page",version:"0.1.34","open-version":!0,"public-url":"/table-page",modules:[{name:"table-page",baseDir:"/home/runner/work/table-page/table-page",description:"A React table page component based on Ant Design, supporting column config, filter, sort and batch operations.",packageName:"@kne/table-page"}]}])}}]);
-//# sourceMappingURL=2298.4bbbaf48.chunk.js.map
